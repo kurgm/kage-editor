4e5c795e8ab40ff1bf21b5b754dcff1e4863ec60
kurgm <gogottt2009-hp@yahoo.co.jp>
2020-06-06 00:50:26 +0900

    setup main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-edit-area">
+        <div className="glyph-area" />
+        <div className="editor-controls">
+        </div>
+      </div>
+      <div className="parts-list-area" />
     </div>
   );
 }
