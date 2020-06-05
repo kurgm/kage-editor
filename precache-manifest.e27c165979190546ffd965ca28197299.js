0b965d66bfd8e57444fe86d5e761068573df6819
kurgm <kurgm@users.noreply.github.com>
2020-06-05 16:35:07 +0000

    deploy: 24fd22a0cac0a4ecb775624b2e5d1cb52da2a083

---
+++ precache-manifest.e27c165979190546ffd965ca28197299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f042b7e37036074aa300bb4dd8c94a22",
+    "url": "/kage-editor/index.html"
+  },
+  {
+    "revision": "ff3fdd53cd65da6b6a66",
+    "url": "/kage-editor/static/css/main.9540ac85.chunk.css"
+  },
+  {
+    "revision": "a6160860ff851c182d77",
+    "url": "/kage-editor/static/js/2.77aea297.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kage-editor/static/js/2.77aea297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3fdd53cd65da6b6a66",
+    "url": "/kage-editor/static/js/main.3e6c655a.chunk.js"
+  },
+  {
+    "revision": "66926905936681612415",
+    "url": "/kage-editor/static/js/runtime-main.1230a81a.js"
+  }
+]);
