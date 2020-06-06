200f9700fab3ab8d4665a43f8b17a3eba847f5ee
kurgm <gogottt2009-hp@yahoo.co.jp>
2020-06-06 21:02:59 +0900

    Add selection actions

--- src/actions/editor.ts
+++ src/actions/editor.ts
@@ -4,4 +4,9 @@ const actionCreator = actionCreatorFactory('EDITOR');
 
 export const editorActions = {
   selectSingle: actionCreator<number>('SELECT_SINGLE'),
+  selectXorSingle: actionCreator<number>('SELECT_XOR_SINGLE'),
+  selectNone: actionCreator('SELECT_NONE'),
+  selectAll: actionCreator('SELECT_ALL'),
+  selectPrev: actionCreator('SELECT_PREV'),
+  selectNext: actionCreator('SELECT_NEXT'),
 };
--- src/reducers/editor.ts
+++ src/reducers/editor.ts
@@ -17,8 +17,43 @@ const initialState: EditorState = {
 };
 
 const editor = reducerWithInitialState(initialState)
-  .case(editorActions.selectSingle, (state, index) => (
-    { ...state, selection: [index] }
-  ));
+  .case(editorActions.selectSingle, (state, index) => ({
+    ...state,
+    selection: [index],
+  }))
+  .case(editorActions.selectXorSingle, (state, index) => ({
+    ...state,
+    selection: state.selection.includes(index)
+      ? state.selection.filter((index2) => index !== index2)
+      : state.selection.concat([index])
+  }))
+  .case(editorActions.selectAll, (state) => ({
+    ...state,
+    selection: state.glyph.map((_gLine, index) => index),
+  }))
+  .case(editorActions.selectNone, (state) => ({
+    ...state,
+    selection: [],
+  }))
+  .case(editorActions.selectPrev, (state) => {
+    if (state.glyph.length === 0) {
+      return { ...state, selection: [] };
+    }
+    const firstSelected = state.selection.length === 0 ? 0 : Math.min(...state.selection);
+    return {
+      ...state,
+      selection: [(firstSelected - 1 + state.glyph.length) % state.glyph.length],
+    };
+  })
+  .case(editorActions.selectNext, (state) => {
+    if (state.glyph.length === 0) {
+      return { ...state, selection: [] };
+    }
+    const firstSelected = state.selection.length === 0 ? -1 : Math.max(...state.selection);
+    return {
+      ...state,
+      selection: [(firstSelected + 1 + state.glyph.length) % state.glyph.length],
+    };
+  });
 
 export default editor;
