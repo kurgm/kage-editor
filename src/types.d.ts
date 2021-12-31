2a372223425b98bbc187de7eacced431bf790632
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2022-01-01 01:06:10 +0900

    Update dependencies

--- src/types.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'memoize-one' {
-  export default function memoizeOne<ResultFn extends (this: any, ...newArgs: any[]) => ReturnType<ResultFn>>(resultFn: ResultFn, isEqual?: (newArgs: Parameters<ResultFn>, lastArgs: Parameters<ResultFn>) => boolean): ResultFn;
-}
