0a624387ac29a10832ae558fe5932e79a7e4b082
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2022-07-23 13:12:42 +0900

    Update to react-scripts@5.0.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 
@@ -10,13 +10,15 @@ import './i18n';
 
 import './index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
