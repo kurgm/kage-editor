bdcd2ab4ab9c674fb66b68699e030bb068102953
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2023-10-15 23:30:42 +0900

    Use preset eslint config of testing-library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -251,33 +251,14 @@ module.exports = {
     },
     {
       files: ['**/__tests__/**/*', '**/*.{spec,test}.*'],
-      extends: ['plugin:vitest/recommended'],
-      // A subset of the recommended rules:
+      extends: [
+        'plugin:vitest/recommended',
+        'plugin:testing-library/react',
+      ],
       rules: {
         'vitest/no-conditional-expect': 'error',
         'vitest/no-interpolation-in-snapshots': 'error',
         'vitest/no-mocks-import': 'error',
-
-        // https://github.com/testing-library/eslint-plugin-testing-library
-        'testing-library/await-async-query': 'error',
-        'testing-library/await-async-utils': 'error',
-        'testing-library/no-await-sync-query': 'error',
-        'testing-library/no-container': 'error',
-        'testing-library/no-debugging-utils': 'error',
-        'testing-library/no-dom-import': ['error', 'react'],
-        'testing-library/no-node-access': 'error',
-        'testing-library/no-promise-in-fire-event': 'error',
-        'testing-library/no-render-in-setup': 'error',
-        'testing-library/no-unnecessary-act': 'error',
-        'testing-library/no-wait-for-empty-callback': 'error',
-        'testing-library/no-wait-for-multiple-assertions': 'error',
-        'testing-library/no-wait-for-side-effects': 'error',
-        'testing-library/no-wait-for-snapshot': 'error',
-        'testing-library/prefer-find-by': 'error',
-        'testing-library/prefer-presence-queries': 'error',
-        'testing-library/prefer-query-by-disappearance': 'error',
-        'testing-library/prefer-screen-queries': 'error',
-        'testing-library/render-result-naming-convention': 'error',
       },
     },
   ],
