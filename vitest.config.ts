2fa0bc71259608fba250878eab4bbc32b312850a
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2025-10-19 18:44:51 +0900

    Update test-related dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    globals: true,
     setupFiles: ['./src/setupTests.ts'],
   },
 });
